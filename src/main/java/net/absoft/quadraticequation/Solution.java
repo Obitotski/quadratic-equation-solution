969c24d756c79054245b2d7df51924eda8fb7826
Obitotski <andrew.obitotski@gmail.com>
2022-04-10 02:18:21 +0300

    Initialised project

---
+++ src/main/java/net/absoft/quadraticequation/Solution.java
@@ -0,0 +1,54 @@
+package net.absoft.quadraticequation;
+
+public class Solution {
+    private double x1;
+    private double x2;
+
+    public Solution solutionOfQuadraticEquation(double a, double b, double c) {
+        Solution solution;
+        double discriminant;
+        discriminant = Math.pow(b, 2) - (4 * a * c); //The formula for determining the discriminant
+        if (discriminant > 0)
+            solution = discriminantIsMoreThanZero(discriminant, a, b, c); //If the discriminant is more than zero
+        else if (discriminant == 0)
+            solution = discriminantIsEqualToZero(discriminant, a, b, c); //If the discriminant is equal to zero
+        else solution = null; //If the discriminant is less than zero
+        return solution;
+    }
+
+    private Solution discriminantIsMoreThanZero(double d, double a, double b, double c) {
+        Solution solution = new Solution();
+        double x1;
+        double x2;
+        x1 = (-b + Math.sqrt(d)) / (2 * a);
+        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        solution.setX1(x1);
+        solution.setX2(x2);
+        return solution;
+    }
+
+    private Solution discriminantIsEqualToZero(double d, double a, double b, double c) {
+        Solution solution = new Solution();
+        double x;
+        x = -b / (2 + a);
+        solution.setX1(x1);
+        solution.setX2(x2);
+        return solution;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+}
---
+++ src/test/java/net/absoft/quadraticequation/SolutionTest.java
@@ -0,0 +1,33 @@
+package net.absoft.quadraticequation;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void testQuadraticEquationSolutionWhenDiscriminantIsLessThanZero() {
+        Solution solution = new Solution();
+        solution = solution.solutionOfQuadraticEquation(3, -4, 2);
+        Assert.assertNull(solution, "Solution of the quadratic equation is not null when discriminant is less than zero");
+    }
+
+    @Test
+    public void testQuadraticEquationSolutionWhenDiscriminantIsMoreThanZero() {
+        Solution solution = new Solution();
+        solution = solution.solutionOfQuadraticEquation(1, -4, -5);
+        Assert.assertEquals(solution.getX1(), 5, "The x1 root of the quadratic equation is incorrect");
+        Assert.assertEquals(solution.getX2(), -1, "The x2 root of the quadratic equation is incorrect");
+    }
+
+    /*
+
+    In case, the discriminant is equal to zero, we`ll have this option
+    @Test
+    public void testQuadraticEquationSolutionWhenDiscriminantIsEqualToZero() {
+        Solution solution = new Solution();
+        Assert.assertEquals(solution.solutionOfQuadraticEquation(3, -4, 2), "Solution is not null when discriminant is less than zero");
+    }
+
+     */
+}
